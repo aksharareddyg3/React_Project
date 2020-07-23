c6c720824fa5c54d033a27d087bf29922af8fdb0
Akshara Gone <aksharareddyg3@gmail.com>
2020-07-23 11:02:32 -0400

    Week3_Part3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from './components/shared/dishes';
+import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
+const store = ConfigureStore();
+
 class App extends Component {
-  constructor(props){
-    super(props);
-      this.state={
-        dishes:DISHES
-      };
-    }
 
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">My Menu</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
+      <Provider store={store}>
+      <BrowserRouter>
+      <div className="App">
+        <Main/>
       </div>
+      </BrowserRouter>
+      </Provider>
     );
   }
 }
